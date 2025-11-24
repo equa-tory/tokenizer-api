ee2982a880a84e55652a71ddd792c9fb2aa87a54
Equa <TheSuspect9980@gmail.com>
2025-11-24 22:34:16 +0300

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DB_URL = os.getenv("DATABASE_URL", "postgresql://ticket_user:your_password@localhost/tickets_db")
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
